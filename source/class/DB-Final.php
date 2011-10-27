b6643e890ead032d3b18a8043a59f91554e75634
Szorstki <s6315@pjwstk.edu.pl>
2011-10-27 20:30:02 +0200

    Zrobiony konstruktor(łączenie z bazą) i singleton, metody trzeba jeszcze omówić

--- source/class/DB-Final.php
+++ source/class/DB-Final.php
@@ -1,12 +1,36 @@
 <?php
 class DB {
 	
-	private $dbname = "biobattleground";
-	private $dbuser = "bbg";
-	private $dbpass = "test123";
-	private $dbhost = "localhost";
+	private $dbname = 'biobattleground';
+	private $dbuser = 'root';
+	private $dbpass = '';
+	private $dbhost = 'localhost';
 	
-	//TODO: Konstruktor, po³¹czenie z baz¹, funkcje do obs³ugi select, insert i update 
+	private static $instance;
+	
+	/* TODO: 
+	 * Konstruktor, po³¹czenie z baz¹ - DONE+singleton,
+	 * funkcje do obs³ugi select - do przedyskutowania ;),
+	 * insert i update
+	 */
+	private function __construct() {
+		$connectionParams = 'mysql:host='.$this->dbhost.';dbname='.$this->dbname;
+		try {
+			// host, dbname, user, pass
+			$this->$db = new PDO($connectionParams, $this->dbuser, $this->dbpass);
+			//return
+		} catch (PDOException $e) {
+			print "Error!: " . $e->getMessage() . "<br/>";
+			die();
+		}
+	}
+	
+	public static function getInstance(){
+		if(!isset($instance)) {
+			$instance = new DB();
+		}
+		return $instance;
+	}
 	
 }
 
