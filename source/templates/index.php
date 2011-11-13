a6d79f0a4ad360fdf61ca1ce4593781898148c2a
Szorstki <s6315@pjwstk.edu.pl>
2011-11-13 16:29:36 +0100

    Poprawienie komunikatu powitalnego (personalizacja)

--- source/templates/index.php
+++ source/templates/index.php
@@ -3,9 +3,15 @@ require_once 'base/page.php';
 	
 class StronaGlowna extends Page {
 	
-	
 	public function WyswietlZawartosc() {
-		echo "Witaj w symulacji organizmów w modelu biologicznym \"Biobattleground\"! <br/><br/>By uzyskać dostęp do wszystkich możliwości systemu należy posiadać własne konto i zalogować się.";
+		echo 'Witaj w symulacji organizmów w modelu biologicznym "BioBattleGround"! <br/><br/>';
+		if (!$_SESSION['zalogowany']) {
+			echo 'By uzyskać dostęp do wszystkich możliwości systemu należy 
+			<a href="register.php">posiadać własne konto</a> i <a href="logowanie.php">zalogować się</a>.';
+		} else {
+			echo '<strong>'.ucfirst($_SESSION['zalogowany']).'</strong>, aby rozpocząć korzystanie z serwisu, wybierz odpowiednią opcję z menu na górze strony.';
+		}
+		
 	}
 }
 
